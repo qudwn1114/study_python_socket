b8f14f0e9a4fd93784b42c9e2ce2d5c1cd2af2be
qudwn1114 <qudwn1114@gmail.com>
2022-03-22 16:24:00 +0900

    feat: group chat

--- socket/client.py
+++ socket/client.py
@@ -1,15 +1,31 @@
 from socket import *
+import threading
 
-port = 8080
+port = 9999
 
 clientSock = socket(AF_INET, SOCK_STREAM)
 clientSock.connect(('127.0.0.1', port))
+nickname = input("Choose your nickname: ")
 
-print('연결 확인 됐습니다.')
+def receive(): 
+    while True: 
+        try: 
+            message = clientSock.recv(1024).decode('utf-8') 
+            if message == 'NICKNAME': clientSock.send(nickname.encode('utf-8')) 
+            else: print(message) 
+        except: 
+            print("An error occured!") 
+            clientSock.close() 
+            break
+def send(): 
+    while True: 
+        message = '{}: {}'.format(nickname, input('')) 
+        clientSock.send(message.encode('utf-8'))
 
-while True:
-    recvData = clientSock.recv(1024)
-    print('받은 데이터 : ', recvData.decode('utf-8'))
+# 멀티 클라이언트용 쓰레드 
+receive_thread = threading.Thread(target=receive) 
+receive_thread.start() 
 
-    sendData = input('>>>')
-    clientSock.send(sendData.encode('utf-8'))
+# 메시지 보내기 
+send_thread = threading.Thread(target=send) 
+send_thread.start()
--- socket/server.py
+++ socket/server.py
@@ -1,20 +1,55 @@
 from socket import *
+import threading
+import time
 
-port = 8080
+port = 9999
 serverSock = socket(AF_INET, SOCK_STREAM)
 
 serverSock.bind(('', port))
-serverSock.listen(1)
+serverSock.listen()
 
-print(f'{port}번 포트로 접속 대기중...')
+clients = []
+nicknames = []
 
-connectionSock, addr = serverSock.accept()
+# 서버가 받은 메시지를 클라이언트 전체에 보내기
+def broadcast(message): 
+    for client in clients: 
+        client.send(message)
 
-print(str(addr),'에서 접속되었습니다.')
 
-while True:
-    sendData = input('>>>')
-    connectionSock.send(sendData.encode('utf-8'))
+def handle(client): 
+    while True: 
+        try: 
+            # 클라이언트로부터 타당한 메시지를 받았는지 확인 
+            message = client.recv(1024) 
+            
+            # 브로드캐스트 함수 동작 
+            broadcast(message) 
+        except: 
+            # 클라이언트가 나갔으면 알림 
+            index = clients.index(client) 
+            clients.remove(client) 
+            client.close() 
 
-    recvData = connectionSock.recv(1024)
-    print('받은 데이터 : ', recvData.decode('utf-8'))
+            nickname = nicknames[index] 
+            broadcast("{} left!\n".format(nickname).encode('utf-8')) 
+            broadcast("{} people in this room!\n".format(len(nicknames)).encode('utf-8')) 
+            nicknames.remove(nickname) 
+            break
+
+def receive(): 
+    while True: 
+        client, address = serverSock.accept() 
+        print("Connected with {}".format(str(address))) 
+        client.send('NICKNAME'.encode('utf-8')) 
+        nickname = client.recv(1024).decode('utf-8') 
+        nicknames.append(nickname) 
+        clients.append(client) 
+        print("Nickname is {}".format(nickname)) 
+        broadcast("{} joined!\n".format(nickname).encode('utf-8')) 
+        broadcast("{} people in this room!\n".format(len(nicknames)).encode('utf-8')) 
+        client.send('Connected to server!'.encode('utf-8')) 
+        thread = threading.Thread(target=handle, args=(client,)) 
+        thread.start()
+
+receive()
